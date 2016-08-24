d8014e5e4ec10fb6062344506ee5cca7bb1bc5db
erickalc <erickalcanttara@gmail.com>
2016-08-24 17:11:00 -0300

    Código de treinamento modificado para cálculo de acurácia e outros.

--- Codigos/ProcessaSinais/trainFeatures.py
+++ Codigos/ProcessaSinais/trainFeatures.py
@@ -6,6 +6,12 @@ import pdb
 import scipy as sp
 import mne
 from scipy import signal
+from sklearn import svm
+from sklearn import preprocessing
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
 
 #metodo para carregar arquivo edf
 #retorna uma matriz numpy. Nas linhas são os canais, nas colunas são os dados
@@ -21,6 +27,7 @@ def computePSD(mChosenChns):
 	f4, psd4 = signal.welch(mChosenChns[3,:],fs=200)
 	myArray = np.concatenate((psd1, psd2, psd3, psd4))
 	#return psd1, psd2, psd3, psd4
+	#pdb.set_trace()
 	return myArray
 	
 def getChosenChannels(edfFileName):
@@ -59,32 +66,92 @@ def thresholdValenceValues(csvFile):
 			thresholdLabels.append(0)
 	return thresholdLabels
 
-		
+def mainMatrixGenerator(edfPathName):
+		#Contador para controle dos vídeos
+		count = 1
+		#Criação da Matriz principal com 20 linhas e 516 colunas
+		mainMatrix = np.zeros(shape = (20 , 516)) # essa quantidade de colunas não deve ser fixa!
+		#Laço para capturar os sinais dos 20 vídeos de cada parte
+		while count <= 20:
+			edfFileName = edfPathName + '/Video' + str(count) + '.edf'
+			mChosenChns = getChosenChannels(edfFileName)
+			
+			psd = computePSD(mChosenChns)
+			
+			#pdb.set_trace()
+			#Adiciona uma nova linha de vídeo a matriz principal 
+			mainMatrix[count - 1] = psd
+			count += 1
+		return mainMatrix
 
 if __name__ == "__main__" :
 	#print 'Hello!'
 
 	labels1FileName = sys.argv[1]
-	edfFileName = sys.argv[2]
+	edfPathName1 = sys.argv[2]
+	
+	labels2FileName = sys.argv[3]
+	edfPathName2 = sys.argv[4]
+	
+	labels3FileName = sys.argv[5]
+	edfPathName3 = sys.argv[6]	
 
 	#labels1 = readCSV(labels1FileName)
+	thresholds1 = thresholdValenceValues(labels1FileName)	
+	mainMatrix1 = mainMatrixGenerator(edfPathName1)
 	
-	thresholds = thresholdValenceValues(labels1FileName)
-	print thresholds	
-	print len(thresholds)
+	thresholds2 = thresholdValenceValues(labels2FileName)	
+	mainMatrix2 = mainMatrixGenerator(edfPathName2)
 	
-	mChosenChns = getChosenChannels(edfFileName)
+	thresholds3 = thresholdValenceValues(labels3FileName)	
+	mainMatrix3 = mainMatrixGenerator(edfPathName3)
 	
-	#psd1, psd2, psd3, psd4 = computePSD(mChosenChns)
-	psd = computePSD(mChosenChns)
+	allLabels = np.concatenate((thresholds1, thresholds2, thresholds3))
+	bigMat = np.concatenate((mainMatrix1, mainMatrix2, mainMatrix3))
 	
-	print len(mChosenChns[0])
+	labelsPlusData = np.zeros((len(allLabels), len(bigMat[0])+1))
 	
+	labelsPlusData[:,0] = allLabels[:]
 	
-	print psd.shape
+	labelsPlusData[:,1:] = bigMat
+	
+	np.random.shuffle(labelsPlusData)
+
+	#pdb.set_trace()
+
+	scaler = preprocessing.StandardScaler().fit(labelsPlusData[:,1:])
+	
+	mTrn = labelsPlusData[0:40,:]
+	mTst = labelsPlusData[40:,:]
+	
+	XTrn = scaler.transform(mTrn[:,1:])
+	XTst = scaler.transform(mTst[:,1:])
+	YTst = mTst[:,0]
+	
+	clf = svm.SVC()
+	clf.fit(XTrn, mTrn[:,0])  
+	#SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0, decision_function_shape=None, degree=3, gamma='auto', kernel='rbf', max_iter=-1, probability=False, random_state=None, shrinking=True, tol=0.001, verbose=False)
+    	Ypredicted = clf.predict(XTst)
+    		
+	acc = accuracy_score(YTst, Ypredicted)
+	
+	f1 = f1_score(YTst, Ypredicted, average='micro')
+	
+	rc = recall_score(YTst, Ypredicted, average='micro')
+	
+	pr = precision_score(YTst, Ypredicted, average='micro')
+	
+	print 'accuracy: '+str(acc)
+	print 'f1 score: '+str(f1)
+	print 'precision: '+str(pr)
+	print 'recall: '+str(rc)
+	
+	#pdb.set_trace()
+	
+	#print len(mChosenChns[0])
+	#print psd.shape
 	#print psd2.shape
 	#print psd3.shape
 	#print psd4.shape
-	
 	#pdb.set_trace()
 	# próximo passo: realizar o treinamento do classificador SVM: http://scikit-learn.org/stable/modules/svm.html
